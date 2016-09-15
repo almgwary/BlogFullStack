26169cfdde4dcbce3e6cfe3f44375c403ebf3ae0
almgwary <almgwary@outlook.com>
2016-09-15 23:24:56 +0200

    add fab to view

--- FrontendAngular/app/root/app.component.ts
+++ FrontendAngular/app/root/app.component.ts
@@ -2,10 +2,12 @@ import {Component} from 'angular2/core';
 import {HomeComponent} from '../home/home.component';
 import {Login} from '../login/login.component';
 import {Signup} from '../signup/signup.component';
+import {Fab} from '../fab/fab.component';
+
 @Component({
     selector: 'my-app',
     templateUrl:'app/root/view/app.view.html',
     styleUrls:['app/root/style/app.styles.css'],
-    directives:[HomeComponent,Login,Signup]
+    directives:[HomeComponent,Login,Signup,Fab]
 })
 export class AppComponent { }
