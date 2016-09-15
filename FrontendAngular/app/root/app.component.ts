0b96c89673b84b5c50d1603220cf2811a1435399
almgwary <almgwary@outlook.com>
2016-09-15 20:47:26 +0200

    add login component to view

--- FrontendAngular/app/root/app.component.ts
+++ FrontendAngular/app/root/app.component.ts
@@ -1,9 +1,10 @@
 import {Component} from 'angular2/core';
-import {HomeComponent} from '../home/home.component'
+import {HomeComponent} from '../home/home.component';
+import {Login} from '../login/login.component'
 @Component({
     selector: 'my-app',
     templateUrl:'app/root/view/app.view.html',
     styleUrls:['app/root/style/app.styles.css'],
-    directives:[HomeComponent]
+    directives:[HomeComponent,Login]
 })
 export class AppComponent { }
