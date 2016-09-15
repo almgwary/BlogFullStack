6d51fee8afbd911eba8984cf0e11ab9d5b83094b
almgwary <almgwary@outlook.com>
2016-09-15 20:58:55 +0200

    add sign up to view

--- FrontendAngular/app/root/app.component.ts
+++ FrontendAngular/app/root/app.component.ts
@@ -1,10 +1,11 @@
 import {Component} from 'angular2/core';
 import {HomeComponent} from '../home/home.component';
-import {Login} from '../login/login.component'
+import {Login} from '../login/login.component';
+import {Signup} from '../signup/signup.component';
 @Component({
     selector: 'my-app',
     templateUrl:'app/root/view/app.view.html',
     styleUrls:['app/root/style/app.styles.css'],
-    directives:[HomeComponent,Login]
+    directives:[HomeComponent,Login,Signup]
 })
 export class AppComponent { }
