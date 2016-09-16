fbf39a792df3304bab80e2e72499aaf3d51c8526
almgwary <almgwary@outlook.com>
2016-09-16 17:10:05 +0200

    add router

--- FrontendAngular/app/root/app.component.ts
+++ FrontendAngular/app/root/app.component.ts
@@ -4,11 +4,21 @@ import {ProfileComponent} from '../profile/profile.component';
 import {Login} from '../login/login.component';
 import {Signup} from '../signup/signup.component';
 import {Fab} from '../fab/fab.component';
+// ROUTER_DIRECTIVES : to user [routerLink] in navigation 
+// RouteConfig : to set router configrations
+import {ROUTER_DIRECTIVES,RouteConfig} from 'angular2/router'
 
 @Component({
     selector: 'my-app',
     templateUrl:'app/root/view/app.view.html',
     styleUrls:['app/root/style/app.styles.css'],
-    directives:[HomeComponent,ProfileComponent,Login,Signup,Fab]
+    directives:[HomeComponent,ProfileComponent,Login,Signup,Fab,ROUTER_DIRECTIVES]
 })
+// set router opjects 
+@RouteConfig([ 
+  {path:'/home',name:'Home',component:HomeComponent,useAsDefault:true},
+  {path:'/profile',name:'Profile',component:ProfileComponent},
+  {path:'/login',name:'Login',component:Login},
+  {path:'/signup',name:'Signup',component:Signup}
+])
 export class AppComponent { }
