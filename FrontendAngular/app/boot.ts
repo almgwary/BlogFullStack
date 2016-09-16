eded9b08da3418813420837ae1515042f55a2f70
almgwary <almgwary@outlook.com>
2016-09-16 14:07:15 +0200

    add http, ruoter and RXjs libraries

--- FrontendAngular/app/boot.ts
+++ FrontendAngular/app/boot.ts
@@ -1,4 +1,9 @@
 import {bootstrap}    from 'angular2/platform/browser'
 import {AppComponent} from './root/app.component'
 
-bootstrap(AppComponent);
+import {ROUTER_PROVIDERS} from 'angular2/router'
+// load http and Rx debendances and inject them in boot strap
+import { HTTP_PROVIDERS } from 'angular2/http'
+import 'rxjs/Rx';
+
+bootstrap(AppComponent,[ROUTER_PROVIDERS,HTTP_PROVIDERS]);   
