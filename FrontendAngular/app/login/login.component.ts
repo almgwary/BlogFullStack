39cfc1f76fca85d8a5aef7cc78255599b01bdee9
almgwary <almgwary@outlook.com>
2016-09-16 15:31:48 +0200

    add user service to login page

--- FrontendAngular/app/login/login.component.ts
+++ FrontendAngular/app/login/login.component.ts
@@ -1,17 +1,31 @@
 import {Component} from 'angular2/core'
+import {UserService} from "../services/user.service"
 
 @Component({
     selector:'login',
     templateUrl:'app/login/view/login.html',
-    styleUrls:['app/login/style/login.css']
+    styleUrls:['app/login/style/login.css'],
+     providers:[UserService]
     
 })
 export class Login {
-    constructor(){}
-    email="";
-    password="";
+    constructor(private userService:UserService){}
+    user = {
+        email:"",
+        password:""
+    }
+    
     login(){
-        console.log("Login:",this.email,this.password);
+        console.log("Login:",this.user);
+        this.userService.login(this.user)
+        .subscribe(
+            user => {
+                console.log("succsfull logedin up ",user);
+            },
+            error => {
+                // handel error here
+            }
+        )
     }
 
 }
