dc46493aa5899be9682efc8d96b56af304a1dd95
almgwary <almgwary@outlook.com>
2016-09-16 23:10:32 +0200

    set fab actions

--- FrontendAngular/app/fab/fab.component.ts
+++ FrontendAngular/app/fab/fab.component.ts
@@ -1,5 +1,6 @@
 import {Component} from 'angular2/core'
-
+import {SharedService}from '../services/shared.service'
+import {Router} from 'angular2/router'
 @Component({
     selector:'fab',
     templateUrl:'app/fab/view/fab.html',
@@ -7,15 +8,36 @@ import {Component} from 'angular2/core'
     
 })
 export class Fab {
-    constructor(){}
+    constructor(private sharedService:SharedService,private router:Router){
+         this.sharedService.fabState.subscribe(
+            fabState => { 
+                    if(fabState=='profile'){
+                     this.state = 'back';                   
+                    }else if (fabState=='home'){
+                          this.state = 'logout'
+                          /*
+                           * check if postForm have text change state to this.state = 'add-post'
+                           * **/
+                   
+                    }
+                }
+        )
+    }
     // [ logout - add-post - back]
     state="back";
 
     onClick(){
         console.log("fab:",this.state);
-        if(this.state == 'logout') this.state = 'add-post';
-        else  if(this.state == 'add-post') this.state = 'back';
-        else this.state = 'logout';
+        if(this.state == 'logout') {
+            // handel log out action
+            this.sharedService.setActiveUser(null);
+        }
+        else  if(this.state == 'add-post')  {
+             // add post action
+        }
+        else   if(this.state == 'back') {
+            this.router.navigate(['Home',{}]);
+        }
     }
 
 }
