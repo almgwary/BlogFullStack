3974ca3787becb7d5403370653ee9ae1ca011899
almgwary <almgwary@outlook.com>
2016-09-16 15:27:06 +0200

    add user service to sign up

--- FrontendAngular/app/signup/signup.component.ts
+++ FrontendAngular/app/signup/signup.component.ts
@@ -1,14 +1,15 @@
 import {Component} from 'angular2/core'
-
+import {UserService} from "../services/user.service"
 @Component({
     selector:'signup',
     templateUrl:'app/signup/view/signup.html',
-    styleUrls:['app/signup/style/signup.css']
+    styleUrls:['app/signup/style/signup.css'],
+     providers:[UserService]
     
 })
 export class Signup {
     avatars = [];
-    constructor(){
+    constructor(private userService:UserService){
         // creat avatars urls
         for(var i=0;i<25;++i)
             this.avatars.push('resources/img/avatars/'+i.toString()+'.png');
@@ -22,6 +23,15 @@ export class Signup {
     
     signup(){
         console.log("signup:",this.user);
+        this.userService.signUp(this.user)
+        .subscribe(
+            user => {
+                console.log("succsfull signed up ",user);
+            },
+            error => {
+                // handel error here
+            }
+        )
     }
 
 }
