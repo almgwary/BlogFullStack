f9b29d9a864bd1000ce19b6697f22a12769b7a50
almgwary <almgwary@outlook.com>
2016-09-16 14:29:00 +0200

    add post service to  home page

--- FrontendAngular/app/home/home.component.ts
+++ FrontendAngular/app/home/home.component.ts
@@ -1,14 +1,25 @@
 import {Component} from 'angular2/core';
 import {Post} from "../post/post.component";
 import {PostForm} from "../postForm/postForm.component"
+import {PostService} from "../services/post.service"
  
 @Component({
     selector:'home',
     templateUrl:'app/home/view/home.view.html',
     styleUrls:['app/home/style/home.style.css'],
-    directives:[Post,PostForm]
+    directives:[Post,PostForm],
+    providers:[PostService]
 })
 export class HomeComponent { 
+  posts=[];
+  errorMessage :any;
+  constructor(private postService:PostService){
+     this.postService.getPosts(null)
+                   .subscribe(
+                     posts => this.posts = posts,
+                     error =>  this.errorMessage = <any>error);
+  
+  }
 
  toggelPostLike($event){
     // [$event.post] is revrence to Dom element
@@ -17,66 +28,5 @@ export class HomeComponent {
      else $event.post.likeCount-=1;
  }
 
- posts =[
-    {"id": "1",
-      "time": 1231648787,
-      "content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standa",
-      "likeCount": 10,
-      "isLike": true,
-      "owner": {
-        "id": "1",
-        "name": "Avatar Lour",
-        "email": "email@fci.com",
-        "avatar_url": "resources/img/avatars/13.png"
-      }
-    },
-    {"id": "1",
-      "time": 1231648787,
-      "content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standa",
-      "likeCount": 1000,
-      "isLike": false,
-      "owner": {
-        "id": "1",
-        "name": "Avatar Lour",
-        "email": "email@fci.com",
-        "avatar_url": "resources/img/avatars/12.png"
-      }
-    },
-    {"id": "1",
-      "time": 1231648787,
-      "content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standa",
-      "likeCount": 0,
-      "isLike": false,
-      "owner": {
-        "id": "1",
-        "name": "Avatar Lour",
-        "email": "email@fci.com",
-        "avatar_url": "resources/img/avatars/18.png"
-      }
-    },
-    {"id": "1",
-      "time": 1231648787,
-      "content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standa",
-      "likeCount": 10,
-      "isLike": true,
-      "owner": {
-        "id": "1",
-        "name": "Avatar Lour",
-        "email": "email@fci.com",
-        "avatar_url": "resources/img/avatars/11.png"
-      }
-    },
-    {"id": "1",
-      "time": 1231648787,
-      "content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standa",
-      "likeCount": 10,
-      "isLike": true,
-      "owner": {
-        "id": "1",
-        "name": "Avatar Lour",
-        "email": "email@fci.com",
-        "avatar_url": "resources/img/avatars/18.png"
-      }
-    }
-];
+  
 }
