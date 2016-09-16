3db29e6e4674aecb790bfb121acd28dbe6906b98
almgwary <almgwary@outlook.com>
2016-09-16 22:47:20 +0200

    inject shared service globaly

--- FrontendAngular/app/boot.ts
+++ FrontendAngular/app/boot.ts
@@ -5,5 +5,6 @@ import {ROUTER_PROVIDERS} from 'angular2/router'
 // load http and Rx debendances and inject them in boot strap
 import { HTTP_PROVIDERS } from 'angular2/http'
 import 'rxjs/Rx';
+import {SharedService}from './services/shared.service'
 
-bootstrap(AppComponent,[ROUTER_PROVIDERS,HTTP_PROVIDERS]);   
+bootstrap(AppComponent,[ROUTER_PROVIDERS,HTTP_PROVIDERS,SharedService]);   
